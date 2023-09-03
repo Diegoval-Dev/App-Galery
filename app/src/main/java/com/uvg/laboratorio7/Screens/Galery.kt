459f396724c911c791dd512fe9d9750b709b0351
Diego <diegopablo012@gmail.com>
2023-09-02 19:36:24 -0600

    Funcionamiento correcto del carrusel

--- app/src/main/java/com/uvg/laboratorio7/Screens/Galery.kt
+++ app/src/main/java/com/uvg/laboratorio7/Screens/Galery.kt
@@ -39,15 +39,17 @@ fun GaleryScreen(navController: NavController, onLogout: () -> Unit){
     GaleryBodyContent(navController = navController, onLogout = onLogout)
 }
 
+val mutableListOfPersons = mutableListOf(ArtImage("Diru","Ruth","(2022)", R.drawable.spiralbetty_1667763462561),
+    ArtImage("Burgues Cat 1", "Diego", "(2021)" , R.drawable.dall_e_2023_04_03_19_56_08___una_foto_realista_de_un_gato_burgu_s_vestido_de_traje_con_un_monoculo_bebiendo_vino),
+    ArtImage("Gurgues Cat 2", "Josue", "(2020)", R.drawable.dall_e_2023_04_03_19_59_43___cat_dressed_in_a_suit_drinking_wine___cinematic__photoshoot__shot_on_25mm_lens__depth_of_field__dof__tilt_blur__shutter_speed_1_1000__f_22__white_bala),
+    ArtImage("Cat Style","Fernando","(1455)", R.drawable.dall_e_2023_04_03_19_59_52___cat_dressed_in_a_suit_drinking_wine___cinematic__photoshoot__shot_on_25mm_lens__depth_of_field__dof__tilt_blur__shutter_speed_1_1000__f_22__white_bala),
+    ArtImage("Capy", "Josh", "(455 B.C.)", R.drawable.dall_e_2023_01_27_09_19_50___una_foto_realista_de_un_capibara_burgu_s_vestido_de_traje_con_un_monoculo_bebiendo_vino),
+    ArtImage("Style Capy", "Andres", "(4155 B.C.)",R.drawable.dall_e_2023_04_03_19_50_13___a_photo_of_a_bourgeois_capybara__dressed_in_18th_century_costume_drinking_wine__cinematic__photoshoot__shot_on_25mm_lens__depth_of_field__dof__tilt_bl)
+)
+
 @Composable
 fun GaleryBodyContent( navController: NavController, onLogout: () -> Unit){
     var numberArt by remember { mutableStateOf( 0) }
-
-    val mutableListOfPersons = mutableListOf(ArtImage("Diru","Ruth","(2022)", R.drawable.spiralbetty_1667763462561) ,
-        ArtImage("Burgues Cat 1", "Diego", "(2021)" , R.drawable.dall_e_2023_04_03_19_56_08___una_foto_realista_de_un_gato_burgu_s_vestido_de_traje_con_un_monoculo_bebiendo_vino)
-    )
-
-
     LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
@@ -55,7 +57,7 @@ fun GaleryBodyContent( navController: NavController, onLogout: () -> Unit){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item{
-            GaleryImage(id = idImage)
+            GaleryImage(id =  mutableListOfPersons[numberArt].idImage, title = mutableListOfPersons[numberArt].name, author = mutableListOfPersons[numberArt].author, year = mutableListOfPersons[numberArt].year)
         }
         item{
             Row(
@@ -64,7 +66,11 @@ fun GaleryBodyContent( navController: NavController, onLogout: () -> Unit){
                     .padding(10.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ){
-                Button(onClick = { /*TODO*/ }) {
+                Button(
+                    onClick = {
+                        numberArt = (numberArt - 1 + mutableListOfPersons.size) % mutableListOfPersons.size
+                    }
+                ) {
                     Text("Anterior")
                 }
                 Button(
@@ -74,7 +80,11 @@ fun GaleryBodyContent( navController: NavController, onLogout: () -> Unit){
                 ) {
                     Text("Logout")
                 }
-                Button(onClick = { /*TODO*/ }) {
+                Button(
+                    onClick = {
+                        numberArt = (numberArt + 1) % mutableListOfPersons.size
+                    }
+                ) {
                     Text("Siguiente")
                 }
             }
@@ -83,7 +93,7 @@ fun GaleryBodyContent( navController: NavController, onLogout: () -> Unit){
 }
 
 @Composable
-fun GaleryImage(@DrawableRes id: Int){
+fun GaleryImage(@DrawableRes id: Int, title: String, author: String, year: String){
     val image = painterResource(id)
 
     Column(
